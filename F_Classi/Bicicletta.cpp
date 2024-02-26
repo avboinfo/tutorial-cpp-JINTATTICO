726b7bbbc9c38277aafac6276b923f2d69870e6a
JINTATTICO <150262609+JINTATTICO@users.noreply.github.com>
2024-02-26 08:37:14 +0000

    update code

--- F_Classi/Bicicletta.cpp
+++ F_Classi/Bicicletta.cpp
@@ -13,6 +13,10 @@ class Bicicletta {
         raggio=10;
         colore="Bianco";
     }
+    Bicicletta(int r, string c) {
+    raggio = r;
+    colore = c;
+    }
     void presentati() {
         cout << "Buongiorno, sono una bici di colore " << colore << " e con le ruote " << raggio << "!" << endl;
     }
@@ -21,6 +25,8 @@ class Bicicletta {
 int main()
 {
     Bicicletta x;
+    Bicicletta y(12, "Rossa");
     x.presentati();
+    y.presentati();
     return 0;
 }
