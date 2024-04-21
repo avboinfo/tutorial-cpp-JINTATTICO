97af74788936582d4ed0c464c3ece27aef6a0fbe
JINTATTICO <150262609+JINTATTICO@users.noreply.github.com>
2024-04-21 09:34:28 +0000

    update code

---
+++ X_BattleShip/main.cpp
@@ -0,0 +1,17 @@
+/*
+** main.cpp - il programma che gioca a Battaglia Navale
+** Butt Abdul Mohid - 20/04/2024
+*/
+
+#include <iostream>
+#include "BattleShip.cpp"
+using namespace std;
+
+int main() {
+
+    cout << "Gioco della Battaglia Navale - Buon divertimento" << endl;
+    BattleShip gioco = BattleShip();
+    gioco.play();
+    cout << "GAME OVER!" << endl;
+
+}
